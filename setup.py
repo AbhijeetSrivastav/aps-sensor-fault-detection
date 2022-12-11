8891b7b8f0e55128de3eed2dcd7a42a429ce331f
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2022-12-11 16:59:57 +0530

    setup file and requirements file added

---
+++ setup.py
@@ -0,0 +1,30 @@
+"Install all required libraries"
+
+from setuptools import find_packages, setup
+from typing import List
+
+
+REQUIREMENTS_FILE_NAME = "requirements.txt"
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements()->List[str]:  
+    "Fetch required libraries from requirements file"  
+    with open(REQUIREMENTS_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement.replace("\n", "") for requirement in requirement_list]
+    
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    
+    return requirement_list
+
+
+setup(
+    name = "aps-sensor-fault-detection",
+    version = "0.0.1",
+    author = "Abhijeet Srivastav",
+    author_email = "abhijeetsrivastav292@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements()
+)
