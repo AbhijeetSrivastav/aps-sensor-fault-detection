b65b498215752d38ecb8f25363f36f2bb9327f42
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2022-12-15 18:46:29 +0530

    Data Ingestion Artifact docs updated

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -6,6 +6,14 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionArtifact:
+    """
+    Artifact for Data Ingestion component
+    ---------------------------------------
+    return:
+    - feature_store_file_path: path of feature store of data ingestion component
+    - train_file_path: path of train file created by Data Ingestion component
+    - test_file_path: path of test file created by Data Ingestion component
+    """
     feature_store_file_path:str
     train_file_path:str
     test_file_path:str
