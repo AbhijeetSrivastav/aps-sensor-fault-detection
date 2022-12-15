233c90c0ab2b5a71a5cad172b485baa9b3e25a5f
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2022-12-16 01:18:05 +0530

    main runner code added

---
+++ main.py
@@ -0,0 +1,13 @@
+"Main file for execution"
+
+
+from sensor.pipeline.training_pipeline import start_training_pipeline
+from sensor.logger import logging
+
+
+if __name__=="__main__":
+    try:
+        logging.info(f"------------Initiating Training Pipeline------------")
+        start_training_pipeline()
+    except Exception as e:
+        print(e)
