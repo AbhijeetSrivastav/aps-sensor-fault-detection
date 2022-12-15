c375ec0423c43654b13a4de17f4766b9f33e3c5e
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2022-12-15 18:38:23 +0530

    Data Ingestion Artifact defined

---
+++ sensor/entity/artifact_entity.py
@@ -0,0 +1,31 @@
+"Contain all the artifacts of the components and pipelines"
+
+
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionArtifact:
+    feature_store_file_path:str
+    train_file_path:str
+    test_file_path:str
+
+
+@dataclass
+class DataValidationArtifact:...
+
+
+@dataclass
+class DataTransformationArtifact:...
+
+
+@dataclass
+class ModelTrainerArtifact:...
+
+
+@dataclass
+class ModelEvaluationArtifact:...
+
+
+@dataclass
+class ModelPusherArtifact:...
