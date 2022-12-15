a8eae41943e8d5a7bc207553ff9d968b40670229
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2022-12-15 17:54:08 +0530

    configuration for sensor package updated

---
+++ sensor/config.py
@@ -0,0 +1,26 @@
+"Configuration for sensor package"
+
+import os
+import json
+import pymongo
+import pandas as pd
+from dataclasses import dataclass
+
+
+@dataclass
+class EnvironmentVariable:
+    """
+    Fetch environment variables from .env file
+    """
+
+    mongo_db_url:str =  os.getenv("MONGO_DB_URL")
+
+
+# instance of environment variable
+env_var = EnvironmentVariable()
+
+# creating MongoDB client
+mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
+
+# declaring the target column
+TARGET_COLUMN = "class"
