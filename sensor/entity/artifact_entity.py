331c533fcf4cd68baee8aecdaa70749ee4551f6f
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2022-12-16 19:48:26 +0530

    Docs udpated

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -10,9 +10,9 @@ class DataIngestionArtifact:
     Artifact for Data Ingestion component
     ---------------------------------------
     return:
-    - feature_store_file_path: path of feature store of data ingestion component
-    - train_file_path: path of train file created by Data Ingestion component
-    - test_file_path: path of test file created by Data Ingestion component
+    - `feature_store_file_path`: path of feature store of data ingestion component
+    - `train_file_path`: path of train file created by Data Ingestion component
+    - `test_file_path`: path of test file created by Data Ingestion component
     """
     feature_store_file_path:str
     train_file_path:str
@@ -25,7 +25,7 @@ class DataValidationArtifact:
     Artifact for Data Validation component
     ---------------------------------------
     return:
-    - report_file_path: path of the validation report
+    - `report_file_path`: path of the validation report
     """
     report_file_path:str
 
