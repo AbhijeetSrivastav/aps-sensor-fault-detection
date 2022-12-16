7e4fdea4ee247d44e5f3b493bae3e7645209b53f
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2022-12-16 19:19:12 +0530

    Data Validation Component added to training pipeline

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -7,6 +7,7 @@ from sensor.logger import logging
 from sensor.exception import SensorException
 from sensor.entity import config_entity
 from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 
 
 def start_training_pipeline():
@@ -22,6 +23,15 @@ def start_training_pipeline():
         data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
 
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+        # Data Validation
+        logging.info(f"---------Initiating Data Validation---------")
+        data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+
+        data_validation = DataValidation(data_validation_config==data_validation_config, data_ingestion_artifact=data_ingestion_artifact)
+
+        data_validation_artifact = data_validation.initiate_data_validation()
+
 
     except Exception as e:
         raise SensorException(e, sys)
