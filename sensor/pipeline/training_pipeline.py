4b1a457cb02985cec4fda0dd7cba369bd923da71
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2022-12-17 16:17:27 +0530

    Data Transformatin Component added to training pipeline

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -8,6 +8,7 @@ from sensor.exception import SensorException
 from sensor.entity import config_entity
 from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
+from sensor.components.data_transformation import DataTransformation
 
 
 def start_training_pipeline():
@@ -31,6 +32,14 @@ def start_training_pipeline():
         data_validation = DataValidation(data_validation_config=data_validation_config, data_ingestion_artifact=data_ingestion_artifact)
 
         data_validation_artifact = data_validation.initiate_data_validation()
+
+        # Data Validation
+        logging.info(f"---------Initiating Data Transformation---------")
+        data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+
+        data_transformation = DataTransformation(data_transformation_config=data_transformation_config, data_ingestion_artifact=data_ingestion_artifact)
+
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
 
 
     except Exception as e:
