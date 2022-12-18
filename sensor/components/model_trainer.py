71352f72c2065344bf0ae7c3f8234f9d00c325ef
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2022-12-18 12:07:47 +0530

    train_model method added to Model Trainer Component

--- sensor/components/model_trainer.py
+++ sensor/components/model_trainer.py
@@ -4,6 +4,7 @@
 import os
 import sys
 from typing import Optional
+import numpy as np
 from xgboost import XGBClassifier
 from sklearn.metrics import f1_score
 from sensor.entity import config_entity
@@ -28,6 +29,25 @@ class ModelTrainer:
             self.model_trainer_config = model_trainer_config
 
             self.data_transformation_artifact = data_transformation_artifact
-            
+
+        except Exception as e:
+            raise SensorException(e, sys)
+
+    def train_model(self, x: np.array, y: np.array)->XGBClassifier:
+        """
+        Creates a XGBClassifier which was selected during the experimentation
+        ------------------------------------------------------------
+        input:
+         - `x`: input feature array
+         - `y`: target feature array
+        ------------------------------------------------------------
+        return: `XGBClassifier`
+        """
+        try: 
+            xgb_clf = XGBClassifier()
+            xgb_clf.fit(x, y)
+
+            return xgb_clf
+
         except Exception as e:
             raise SensorException(e, sys)
