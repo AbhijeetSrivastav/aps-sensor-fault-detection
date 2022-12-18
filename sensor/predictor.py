d6270f218645571bebd25bc677de975157d68dec
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2022-12-18 21:32:35 +0530

    Get latest dir method added to Model Resolver

--- sensor/predictor.py
+++ sensor/predictor.py
@@ -15,7 +15,7 @@ class ModelResolver:
     Model Resolver 
     ---------------------------------------------------------
     input:
-     - `model_registry`: directory containing model t
+     - `model_registry`: directory containing all sub directories of the models trained till now in  artifacts of model training component
      - `model_dir_name`: directory containing latest model within `model_registry`
      - `transformer_dir_name`: directory containing artifact of data transformation component
      - `target_encoder_dir_name`: directory containing the target encoder object which is artifact of data transformation component
@@ -36,3 +36,35 @@ class ModelResolver:
         except Exception as e:
             raise SensorException(e, sys)
 
+    def get_latest_dir(self)->Optional[str]:
+        """
+        Returns latest model directory if their any
+        ---------------------------------------------------------
+        input:
+         - `None`
+        ---------------------------------------------------------
+         return: `None` or `latest_dir_name`
+        """
+        try:
+            # Creating list of all the sub directories in the model_registry folder of model training artifact
+            dir_names = os.listdir(self.model_registry)
+
+            # If no sub directory that means no saved model object
+            if len(dir_names) == 0:
+                return None
+            
+
+            # Convert str name of subdir to int for all subdirs
+            dir_names = list(map(int, dir_names))
+
+
+            # Get the latest one as we saved our subdirs as named by date time format
+            latest_dir_name = max(dir_names)
+
+            # Get absolute path to our latest subdir
+            latest_dir_name = os.path.join(self.model_registry, f"{latest_dir_name}")
+
+            return latest_dir_name
+
+        except Exception as e:
+            raise SensorException(e, sys) 
